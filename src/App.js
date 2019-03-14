b1317999106904b8aecdb66aab4e093a75168f59
Amit S Namboothiry <me@amitsn.com>
2019-03-13 23:48:07 -0700

    Client side baby steps

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Navbar } from "react-bootstrap";
+import Routes from "./Routes";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img width="256" height="256" src="/dp_big.png" alt="logo" />
-          <h1>NAADAN <span>CHORDS</span></h1>
-          <small>GUITAR CHORDS AND TABS OF MALAYALAM SONGS</small>
-        </header>
+      <div className="App container">
+        <Navbar fluid="true" collapseOnSelect>
+          <Navbar.Brand>
+            <Link to="/">HEADER</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar>
+        <Routes />
       </div>
     );
-  }
+  }  
 }
 
 export default App;
---
+++ src/containers/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>NAADAN CHORDS</h1>
+          <p>Guitar chords and tabs</p>
+        </div>
+      </div>
+    );
+  }
+}
