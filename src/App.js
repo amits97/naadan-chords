3d1ef26736e8a8fb26f58c45af832995af524aad
Amit S Namboothiry <me@amitsn.com>
2019-03-15 11:06:07 -0700

    Added navbar header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,55 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { Navbar } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { Navbar, Nav } from "react-bootstrap";
 import Routes from "./Routes";
+
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      navExpanded: false,
+    };
+  }
+
+  setNavExpanded = (expanded) => {
+    this.setState({
+      navExpanded: expanded
+    });
+  }
+
+  closeNav = () => {
+    this.setState({
+      navExpanded: false
+    });
+  }
+
   render() {
     return (
-      <div className="App container">
-        <Navbar fluid="true" collapseOnSelect>
-          <Navbar.Brand>
-            <Link to="/">HEADER</Link>
-          </Navbar.Brand>
-          <Navbar.Toggle />
+      <div className="App">
+        <Navbar fluid="true" expand="lg" sticky="top" variant="dark" onToggle={this.setNavExpanded} expanded={this.state.navExpanded}>
+          <div className="container">
+            <Navbar.Brand>
+              <Link to="/">
+                <img src="/logo.svg" alt="logo" />
+                <p>NAADAN<span>CHORDS</span></p>
+              </Link>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+            <Navbar.Collapse className="justify-content-end">
+              <Nav>
+                <LinkContainer exact to="/">
+                  <a href="#/" className="nav-link" onClick={this.closeNav}>Home</a>
+                </LinkContainer>
+                <LinkContainer exact to="/about">
+                  <a href="#/" className="nav-link" onClick={this.closeNav}>About</a>
+                </LinkContainer>
+              </Nav>
+            </Navbar.Collapse>
+          </div>
         </Navbar>
         <Routes />
       </div>
