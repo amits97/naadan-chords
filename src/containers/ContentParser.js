2f081f0dea5ea2e9c718ced9382deca9a5993479
Amit S Namboothiry <me@amitsn.com>
2019-03-22 20:27:17 -0700

    Lots of content parsing

--- src/containers/ContentParser.js
+++ src/containers/ContentParser.js
@@ -1,10 +1,42 @@
 import React, { Component } from "react";
+import { Tabs, Tab } from "react-bootstrap";
 import "./ContentParser.css";
 
 export default class ContentParser extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      leadTabs: null
+    }
+  }
+
   stripHtml = (html) => {
     var doc = new DOMParser().parseFromString(html, 'text/html');
     return doc.body.textContent || "";
+  }
+
+  renderSongMeta = () => {
+    const songMetaRegExp = /{start_song_meta}\n([\s\S]*?)\n{end_song_meta}/gim;
+    const boldRegExp = /{start_bold}([\s\S]*?){end_bold}/gim;
+
+    let content = this.stripHtml(this.props.content);
+
+    //get meta details
+    let song_meta_matches = songMetaRegExp.exec(content);
+    if(song_meta_matches && song_meta_matches.length > 1) {
+      let song_meta = "<div class='meta'><p>" + song_meta_matches[1] + "</p></div>";
+
+      //replace bold
+      song_meta = song_meta.replace(boldRegExp, (match, p1) => {
+        return (`<b>${p1}</b>`);
+      });
+
+      return {__html: song_meta};
+    }
+  }
+
+  renderLeadTabs = () => {
+    return {__html: this.state.leadTabs};
   }
 
   parseContent = () => {
@@ -13,24 +45,43 @@ export default class ContentParser extends Component {
     const italicRegExp = /{start_italic}([\s\S]*?){end_italic}/gim;
     const headingRegExp = /{start_heading}([\s\S]*?){end_heading}/gim;
     const strummingRegExp = /{start_strumming}([\s\S]*?){end_strumming}/gim;
-    const chordsInStrumming = /\[([\s\S]*?)\]/gim;
+    const chordsInStrummingRegExp = /\[([\s\S]*?)\]/gim;
+    const leadTabsRegExp = /{start_lead_tabs}\n([\s\S]*?)\n{end_lead_tabs}/gim;
+    const songMetaRegExp = /{start_song_meta}\n([\s\S]*?)\n{end_song_meta}/gim;
+    const separatorRegExp = /{separator}/gim;
 
     //Chords regex
     const notes = "[CDEFGAB]";
+    const tabBeginning = "(?!\\|)";
     const chords = "(maj7|maj|min7|min|sus2|sus4|m7|m6add9|m7sus2|add9|m|5)?";
     const sharp = "(#)?";
-    const chordsRegex = new RegExp("\\b" + notes + chords + "\\b" + sharp + chords, "g");
+    const chordsRegex = new RegExp("\\b" + notes  + chords + "\\b" + sharp + chords + tabBeginning, "g");
 
     let content = this.stripHtml(this.props.content);
 
+    //remove song meta
+    content = content.replace(songMetaRegExp, (match, p1) => {
+      return "";
+    });
+
     //replace tabs
     content = content.replace(tabRegExp, (match, p1) => {
-      return (`<div class="tabs ignore-chords">${p1}</div>`);
+      return (`<div class="tabs">${p1}</div>`);
     });
 
     //replace bold
     content = content.replace(boldRegExp, (match, p1) => {
       return (`<b>${p1}</b>`);
+    });
+
+    //remove lead tabs and save to lead tab state
+    content = content.replace(leadTabsRegExp, (match, p1) => {
+      if(this.state.leadTabs === null) {
+        this.setState({
+          leadTabs: p1
+        });
+      }
+      return("");
     });
 
     //replace italic
@@ -43,11 +94,9 @@ export default class ContentParser extends Component {
       return (`<h4>${p1}</h4>`);
     });
 
-    //replace strumming
-    content = content.replace(strummingRegExp, (match, p1) => {
-      return (`<span class="ignore-chords">${p1.replace(chordsInStrumming, (match) => {
-        return (`<span class="override-chords">${match}</span>`);
-      })}</span>`);
+    //replace separator
+    content = content.replace(separatorRegExp, (match, p1) => {
+      return (`<hr />`);
     });
 
     //replace chords
@@ -55,12 +104,43 @@ export default class ContentParser extends Component {
       return (`<span class="chord">${match}</span>`);
     });
 
+    //replace strumming
+    content = content.replace(strummingRegExp, (match, p1) => {
+      return (`<span class="ignore-chords">${p1.replace(chordsInStrummingRegExp, (match) => {
+        return (`<span class="override-chords">${match}</span>`);
+      })}</span>`);
+    });
+
     return {__html: content};
+  }
+
+  renderTabs = () => {
+    let { leadTabs } = this.state;
+
+    if(leadTabs !== null) {
+      return (
+        <Tabs defaultActiveKey="chords">
+          <Tab eventKey="chords" title="CHORDS">
+            <div className="pl-3 pr-3" dangerouslySetInnerHTML={ this.parseContent() } />
+          </Tab>
+          <Tab eventKey="tabs" title="LEAD TABS">
+            <div className="pl-3 pr-3 ignore-chords" dangerouslySetInnerHTML={ this.renderLeadTabs() } />
+          </Tab>
+        </Tabs>
+      );
+    } else {
+      return (
+        <div dangerouslySetInnerHTML={ this.parseContent() } />
+      )
+    }
   }
 
   render() {
     return (
-      <div className="ContentParser" dangerouslySetInnerHTML={ this.parseContent() } />
+      <div className="ContentParser">
+        <div dangerouslySetInnerHTML={ this.renderSongMeta() } />
+        { this.renderTabs() }
+      </div>
     );
   }
 }
