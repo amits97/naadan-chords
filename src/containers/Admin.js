b7cffe9838733303930eb25fc4009a5a85630da7
Amit S Namboothiry <nambooth@amazon.com>
2019-03-28 14:03:16 -0700

    Fix link bold stylings

--- src/containers/Admin.js
+++ src/containers/Admin.js
@@ -190,7 +190,7 @@ export default class Admin extends Component {
                   <ListGroup.Item key={i}>
                     <Form.Check type="checkbox" className="checkbox" onChange={(event) => this.addPostToDelete(event, post.postId)} checked={this.state.postsToBeDeleted.indexOf(post.postId) !== -1} />
                     <LinkContainer exact to={`/admin/edit-post/${post.postId}`}>
-                      <a href="#/">{ post.title }</a>
+                      <a href="#/" className="text-primary">{ post.title }</a>
                     </LinkContainer>
                   </ListGroup.Item>
                 );
