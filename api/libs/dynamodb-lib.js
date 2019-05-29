18fb7ed029e50be228b43c29e500acba5be0fa81
Amit S Namboothiry <me@amitsn.com>
2019-05-28 21:36:20 -0700

    Use HTTP keep-alive for DynamoDB operations

--- api/libs/dynamodb-lib.js
+++ api/libs/dynamodb-lib.js
@@ -1,4 +1,17 @@
-import AWS from "aws-sdk";
+const AWS = require("aws-sdk");
+const https = require("https");
+const sslAgent = new https.Agent({
+  keepAlive: true,
+  maxSockets: 50,
+  rejectUnauthorized: true
+});
+sslAgent.setMaxListeners(0);
+
+AWS.config.update({
+  httpOptions: {
+    agent: sslAgent
+  }
+});
 
 export function call(action, params) {
   const dynamoDb = new AWS.DynamoDB.DocumentClient();
