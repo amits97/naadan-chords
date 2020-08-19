8fade8dbfb3df0a61105dca35d319af5aa641663
Amit S Namboothiry <me@amitsn.com>
2020-08-19 02:29:26 -0700

    Chords popup now shows carousel of multiple positions

--- src/containers/ChordsPopup.js
+++ src/containers/ChordsPopup.js
@@ -1,142 +1,163 @@
 import React, { Component } from "react";
-import { OverlayTrigger, Popover } from "react-bootstrap";
+import { Carousel} from "react-bootstrap";
 import * as vexchords from "vexchords";
 import { findGuitarChord } from "chord-fingering";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSyncAlt, faExclamationTriangle } from "@fortawesome/free-solid-svg-icons";
+import PopoverStickOnHover from "../components/PopoverStickOnHover";
 import "./ChordsPopup.css";
 
 export default class ChordsPopup extends Component {
   constructor(props) {
     super(props);
 
+    this.renderedChordElements = [];
+
     this.state = {
       popoverOpen: false,
-      chordElement: null
     };
   }
 
-  getChordElement = (chordName) => {
-    if(this.state.chordElement) {
-      return this.state.chordElement;
+  getChordElements = (chordName) => {
+    if(this.renderedChordElements.length > 0) {
+      return this.renderedChordElements;
     } else {
       let chord = findGuitarChord(chordName);
-      let chordElement = document.createElement("div");
-      let chordPosition = [];
-      let lowestPosition = 12;
-      let barre = [];
+      let chordElements = [];
+      let chordPositions = [];
+      let lowestPositions = [];
+      let barres = [];
+      let validFingeringFound = false;
 
       if(chord) {
-        let positionString = chord.fingerings[0].positionString;
+        chord.fingerings.forEach((fingering) => {
+          let chordPosition = [];
+          let lowestPosition = 12;
+          let highestPosition = 0;
+          let barre = [];
+          let positionString = fingering.positionString;
 
-        if(positionString.length > 6) {
-          let validFingeringFound = false;
+          if (positionString.length !== 6) {
+            return;
+          } else {
+            validFingeringFound = true;
+          }
 
-          for(let i = 1; i < chord.fingerings.length; i++) {
-            positionString = chord.fingerings[i].positionString;
-            if(positionString.length > 6) {
-              continue;
-            } else {
-              validFingeringFound = true;
-              break;
+          for(let i = 0; i < positionString.length; i++) {
+            let position = positionString[i];
+
+            if(position !== "x" && parseInt(position) > highestPosition) {
+              highestPosition = parseInt(position);
+            }
+
+            if(position !== "x" && parseInt(position) < lowestPosition) {
+              lowestPosition = parseInt(position) - 1;
             }
           }
 
-          if(!validFingeringFound) {
+          if (highestPosition - lowestPosition > 5) {
             return;
           }
-        }
+  
+          lowestPosition = lowestPosition < 0 ? 0 : lowestPosition;
 
-        for(let i = 0; i < positionString.length; i++) {
-          let position = positionString[i];
-          if(position !== "x" && parseInt(position) < lowestPosition) {
-            lowestPosition = parseInt(position) - 1;
-          }
-        }
+          for(let i = 1; i <= positionString.length; i++) {
+            let reverseIndex = positionString.length-i;
+            let fretPosition = positionString[reverseIndex];
+  
+            if(fretPosition !== "x") {
+              fretPosition = parseInt(fretPosition) - lowestPosition;
+            }
 
-        lowestPosition = lowestPosition < 0 ? 0 : lowestPosition;
-
-        for(let i = 1; i <= positionString.length; i++) {
-          let reverseIndex = positionString.length-i;
-          let fretPosition = positionString[reverseIndex];
-
-          if(fretPosition !== "x") {
-            fretPosition =  parseInt(fretPosition) - lowestPosition;
+            chordPosition.push([i, fretPosition]);
           }
 
-          chordPosition.push([i, fretPosition]);
-        }
-
-        if(chord.fingerings[0].barre) {
-          let fromString = 6 - chord.fingerings[0].barre.stringIndices[0];
-          let toString = 6 - (chord.fingerings[0].barre.stringIndices.slice(-1).pop());
-
-          if(fromString - toString > 2) {
-            barre.push({
-              fromString: fromString,
-              toString: toString,
-              fret: chord.fingerings[0].barre.fret - lowestPosition
-            });
-
-            let i = chordPosition.length;
-            while(i--) {
-              if(chordPosition[i][1] === chord.fingerings[0].barre.fret  - lowestPosition) {
-                chordPosition.splice(i, 1);
+          if(fingering.barre) {
+            let fromString = 6 - fingering.barre.stringIndices[0];
+            let toString = 6 - (fingering.barre.stringIndices.slice(-1).pop());
+  
+            if(fromString - toString > 2) {
+              barre.push({
+                fromString: fromString,
+                toString: toString,
+                fret: fingering.barre.fret - lowestPosition
+              });
+  
+              let i = chordPosition.length;
+              while(i--) {
+                if(chordPosition[i][1] === fingering.barre.fret  - lowestPosition) {
+                  chordPosition.splice(i, 1);
+                }
               }
             }
           }
-        }
+
+          chordPositions.push(chordPosition);
+          lowestPositions.push(lowestPosition);
+          barres.push(barre);
+        });
+
+        if (!validFingeringFound) return;
       } else {
         return;
       }
 
-      vexchords.draw(chordElement, {
-        chord: chordPosition,
-        position: lowestPosition + 1,
-        barres: barre
-      }, {
-        width: 120,
-        height: 140,
-        fontFamily: "'DINNextLTPro-Regular', 'Helvetica Neue', sans-serif",
-        defaultColor: "#212529"
-      });
+      for (let i = 0; i < chordPositions.length; i++) {
+        let chordElement = document.createElement("div");
 
-      this.setState({
-        chordElement: chordElement
-      });
+        vexchords.draw(chordElement, {
+          chord: chordPositions[i],
+          position: lowestPositions[i] + 1,
+          barres: barres[i]
+        }, {
+          width: 120,
+          height: 140,
+          fontFamily: "'DINNextLTPro-Regular', 'Helvetica Neue', sans-serif",
+          defaultColor: "#212529"
+        });
 
-      return chordElement;
+        chordElements.push(chordElement);
+      }
+
+      this.renderedChordElements = chordElements;
+      return chordElements;
     }
   }
 
   renderchordPopover = (chordName) => {
     if(this.state.popoverOpen) {
-      let chordElement = this.getChordElement(chordName);
+      let chordElements = this.getChordElements(chordName);
 
-      if(chordElement) {
+      if(chordElements && chordElements.length > 0) {
         return (
-          <Popover id="popover-basic" className="ChordsPopup">
-            <div dangerouslySetInnerHTML={{__html: chordElement.innerHTML}}></div>
-          </Popover>
+          <div className="ChordsPopup">
+            <Carousel interval={null}>
+              {
+                chordElements.map((chordElement, index) => {
+                  return (
+                    <Carousel.Item key={index}>
+                      <div className="chord-diagram" dangerouslySetInnerHTML={{__html: chordElement.innerHTML}}></div>
+                    </Carousel.Item>
+                  );
+                })
+              }
+            </Carousel>
+          </div>
         );
       } else {
         return (
-          <Popover id="popover-basic" className="ChordsPopup p-2">
-            <div style={{width: "120px", height: "140px"}}>
-              <span className="error">
-                <FontAwesomeIcon icon={faExclamationTriangle} />
-              </span>
-            </div>
-          </Popover>
+          <div className="ChordsPopup p-2" style={{width: "120px", height: "140px"}}>
+            <span className="error">
+              <FontAwesomeIcon icon={faExclamationTriangle} />
+            </span>
+          </div>
         );
       }
     } else {
       return (
-        <Popover id="popover-basic" className="ChordsPopup p-2">
-          <div style={{width: "120px", height: "140px"}}>
-            <FontAwesomeIcon icon={faSyncAlt} className="spinning" />
-          </div>
-        </Popover>
+        <div className="ChordsPopup p-2" style={{width: "120px", height: "140px"}}>
+          <FontAwesomeIcon icon={faSyncAlt} className="spinning" />
+        </div>
       );
     }
   }
@@ -151,11 +172,16 @@ export default class ChordsPopup extends Component {
     let {chordName} = this.props;
 
     return(
-      <OverlayTrigger trigger={["hover", "focus"]} placement="auto" overlay={this.renderchordPopover(chordName)} onEntered={this.openPopover} delay={{show: 0, hide: 100}}>
+      <PopoverStickOnHover
+          component={this.renderchordPopover(chordName)}
+          placement="auto"
+          onMouseEnter={this.openPopover}
+          delay={0}
+      >
         <span className="chord popup-trigger">
           { chordName }
         </span>
-      </OverlayTrigger>
+      </PopoverStickOnHover>
     );
   }
 }
