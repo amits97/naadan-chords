3fa51aaf37160efd47ab895602e221375cd6feb1
Amit S Namboothiry <me@amitsn.com>
2020-09-27 15:35:41 -0700

    Bug fix - Do not new account email on password change

--- api/account/post-signup.js
+++ api/account/post-signup.js
@@ -2,19 +2,23 @@ import * as emailLib from "../libs/email-lib";
 
 exports.handler = (event, context, callback) => {
   try {
-    let email = event.request.userAttributes.email;
+    if (event.triggerSource === "PostConfirmation_ConfirmSignUp") {
+      let email = event.request.userAttributes.email;
   
-    const title = `Naadan Chords - Congratulations on your new account`;
-    const message = `
-      <p>Hey ${event.request.userAttributes.name},</p>
-      <p>We are writing to let you know that your account has been created. Congratulations on your new Naadan Chords account.</p>
-      <p>Thanks for signing up!</p>
-    `;
-    const textMessage = `Naadan Chords - Congratulations on your new account`;
-  
-    emailLib.syncSendEmail(title, message, textMessage, email, function(status) {
+      const title = `Naadan Chords - Congratulations on your new account`;
+      const message = `
+        <p>Hey ${event.request.userAttributes.name},</p>
+        <p>We are writing to let you know that your account has been created. Congratulations on your new Naadan Chords account.</p>
+        <p>Thanks for signing up!</p>
+      `;
+      const textMessage = `Naadan Chords - Congratulations on your new account`;
+
+      emailLib.syncSendEmail(title, message, textMessage, email, function(status) {
+        context.done(null, event);
+      });
+    } else {
       context.done(null, event);
-    });
+    }
   } catch (e) {
     event.error = e;
     context.done(null,event);
