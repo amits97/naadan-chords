10add0e9f08edd3c87231086c4ec18e5d968023a
Amit S Namboothiry <me@amitsn.com>
2020-10-11 09:38:00 +0530

    Fix CodeQL warnings

--- src/libs/url-lib.js
+++ src/libs/url-lib.js
@@ -1,5 +1,5 @@
 export function getUrlParameter(name) {
-  name = name.replace(/[[]/, '\\[').replace(/[\]]/, '\\]');
+  name = name.replace(/[[]/g, '\\[').replace(/[\]]/g, '\\]');
   let regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
   let results = regex.exec(window.location.search);
   return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
@@ -7,9 +7,9 @@ export function getUrlParameter(name) {
 
 export function insertUrlParam(key, value) {
   if (window.history.pushState) {
-      let searchParams = new URLSearchParams(window.location.search);
-      searchParams.set(key, value);
-      let newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + '?' + searchParams.toString();
-      window.history.pushState({path: newurl}, '', newurl);
+    let searchParams = new URLSearchParams(window.location.search);
+    searchParams.set(key, value);
+    let newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + '?' + searchParams.toString();
+    window.history.pushState({path: newurl}, '', newurl);
   }
 }
