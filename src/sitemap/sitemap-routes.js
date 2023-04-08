7a719946c479522cc05b4ba29848574793beffdd
Amit S Namboothiry <me@amitsn.com>
2023-04-08 20:52:50 +0530

    Update sitemap to include /request

--- src/sitemap/sitemap-routes.js
+++ src/sitemap/sitemap-routes.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import { Route } from 'react-router';
- 
+import React from "react";
+import { Route } from "react-router";
+
 export default (
   <Route>
-    <Route path='/:id' />
-    <Route path='/page/:number' />
-    <Route path='/category/malayalam' />
-    <Route path='/category/malayalam/page/:number' />
-    <Route path='/category/tamil' />
-    <Route path='/category/tamil/page/:number' />
-    <Route path='/category/hindi' />
-    <Route path='/author/:userName' />
-    <Route path='/author/:userName/page/:number' />
-    <Route path='/album/:album' />
-    <Route path='/about' />
-    <Route path='/privacy-policy' />
-	</Route>
+    <Route path="/:id" />
+    <Route path="/page/:number" />
+    <Route path="/category/malayalam" />
+    <Route path="/category/malayalam/page/:number" />
+    <Route path="/category/tamil" />
+    <Route path="/category/tamil/page/:number" />
+    <Route path="/category/hindi" />
+    <Route path="/author/:userName" />
+    <Route path="/author/:userName/page/:number" />
+    <Route path="/album/:album" />
+    <Route path="/about" />
+    <Route path="/request" />
+    <Route path="/privacy-policy" />
+  </Route>
 );
