fdbf4b9332a18ecf4055a37239c81463506921bb
Amit S Namboothiry <me@amitsn.com>
2023-08-09 19:35:44 +0530

    Deploy dependency updates

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import Amplify from "aws-amplify";
+import { Amplify } from "aws-amplify";
 import config from "./config";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
