9f7bfabe3863d60efe57c567ea6a59dc34abc8f4
Amit S Namboothiry <me@amitsn.com>
2023-10-24 11:34:19 +0530

    Update youtube embed css

--- src/components/YouTubeEmbed.js
+++ src/components/YouTubeEmbed.js
@@ -4,19 +4,13 @@ import "./YouTubeEmbed.css";
 const YouTubeEmbed = ({ youtubeId, onLoad }) => {
   return (
     <div className="YouTubeEmbed">
-      <div className="video-wrapper">
-        <div className="video-container">
-          <iframe
-            title={`video-${youtubeId}`}
-            src={`https://www.youtube.com/embed/${youtubeId}`}
-            frameBorder="0"
-            allowFullScreen
-            onLoad={onLoad}
-            width="702"
-            height="425"
-          ></iframe>
-        </div>
-      </div>
+      <iframe
+        title={`video-${youtubeId}`}
+        src={`https://www.youtube.com/embed/${youtubeId}`}
+        frameBorder="0"
+        allowFullScreen
+        onLoad={onLoad}
+      ></iframe>
     </div>
   );
 };
