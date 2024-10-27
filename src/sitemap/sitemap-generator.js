fbf436db1c319f230eee9d3f081ccfa28db62622
Amit S Namboothiry <me@amitsn.com>
2024-10-27 16:56:09 +0530

    Update babel

--- src/sitemap/sitemap-generator.js
+++ src/sitemap/sitemap-generator.js
@@ -1,5 +1,5 @@
-require("babel-register")({
-  presets: ["es2015", "react"],
+require("@babel/register")({
+  presets: ["@babel/preset-env", "@babel/preset-react"],
 });
 
 const { generateDelayPromise } = require("../libs/utils");
