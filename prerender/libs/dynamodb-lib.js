910deb87cfc2e44e57bfc4050928aa7318b0db17
Amit S Namboothiry <me@amitsn.com>
2026-01-24 14:36:44 +0530

    Update prerender

--- prerender/libs/dynamodb-lib.js
+++ prerender/libs/dynamodb-lib.js
@@ -1,7 +1,28 @@
-import AWS from "aws-sdk";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {
+  DynamoDBDocumentClient,
+  GetCommand,
+  PutCommand,
+} from "@aws-sdk/lib-dynamodb";
 
-export function call(action, params) {
-  const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const client = new DynamoDBClient({ region: process.env.AWS_REGION });
+const dynamoDb = DynamoDBDocumentClient.from(client, {
+  marshallOptions: {
+    removeUndefinedValues: true,
+  },
+});
 
-  return dynamoDb[action](params).promise();
+const commandMap = {
+  get: GetCommand,
+  put: PutCommand,
+};
+
+export async function call(action, params) {
+  const CommandClass = commandMap[action];
+  if (!CommandClass) {
+    throw new Error(`Unknown action: ${action}`);
+  }
+
+  const command = new CommandClass(params);
+  return dynamoDb.send(command);
 }
